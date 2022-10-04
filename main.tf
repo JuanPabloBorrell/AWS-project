234f8cd29754e1aa6c80c18ad4b7f524fb410a74
JPB1980 <juanpabloborrell@gmail.com>
2022-10-04 13:39:09 +0200

    change in main.tf tags

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-access_key = var.my_access_key
-secret_key = var.my_secret_key
-region = var.region
+    access_key = var.my_access_key
+    secret_key = var.my_secret_key
+    region = var.region
 }
 
 resource "aws_instance" "web" {
-ami = "ami-05ff5eaef6149df49"
-instance_type = "t2.micro"
+    ami = "ami-05ff5eaef6149df49"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "JPB"
+    }
 }
