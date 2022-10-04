6681a1aa638d28ce91ac94c9db2a332790a52dfe
JPB1980 <juanpabloborrell@gmail.com>
2022-10-04 13:32:49 +0200

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+access_key = var.my_access_key
+secret_key = var.my_secret_key
+region = var.region
+}
+
+resource "aws_instance" "web" {
+ami = "ami-05ff5eaef6149df49"
+instance_type = "t2.micro"
+}
